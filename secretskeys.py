b04d6c990b7a566d0fd0a38ffb94125b275e87a6
eddym <eddyats11"gmail.com>
2021-11-16 09:33:22 +0300

    credential

---
+++ secretskeys.py
@@ -0,0 +1,5 @@
+
+consumer_key = "XRlloheKWmg9UEdBZ83VmAQ0p"
+consumer_secret = "z0HGvGTojxEqXQVArAuNaR3N2X6uRlpNl3zxbTvOSsw16jAzFL"
+access_token = "1168837684716085248-0at4KJnfReuLNvGeda7jCaoT9HcwLO"
+access_token_secret = "JsU4CV96vzCMayKdioIp4UJO1G7qKVadcJNQ4OhHbkxMx"
