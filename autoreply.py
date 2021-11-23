9472b72958e4c86ea6d34a876e57edaaae48e92b
eddym <eddyats11"gmail.com>
2021-11-23 13:41:26 +0300

    autoreply updated

--- autoreply.py
+++ autoreply.py
@@ -7,9 +7,11 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 tweetNumber = 20
-q = "eddymzae"
+q = "can we meet"
 api = create_api()
-tweets = list(tweepy.Cursor(api.search_tweets, q, tweet_mode='extended').items(tweetNumber))
+since_id=1
+tweets = list(tweepy.Cursor(api.search_tweets, q, tweet_mode='extended').items(tweetNumber)) and list(tweepy.Cursor(api.mentions_timeline, since_id=since_id).items())
+
 
 
 def crashBot():
@@ -17,10 +19,11 @@ def crashBot():
 
         try:
 
-            if q in tweet.full_text.lower():
-                print(str(tweet.id) + '-' + tweet.full_text)
-                api.update_status("@" + tweet.user.screen_name + "Please DM me")
+            if q in tweet.text.lower():
+                print(str(tweet.id) + '-' + tweet.text)
+                api.update_status("@" + tweet.user.screen_name + "   Please DM me")
                 api.retweet(tweet.id)
+                tweet.favorite()
                 print("done!")
                 time.sleep(30)
         except tweepy.TweepyException as e:
